170bb56dcec2efa0c80c555c775fd7a1ce7757cf
27freeman <96946280+27freeman@users.noreply.github.com>
2025-05-22 18:05:57 +0100

    Add files via upload

--- TweetData.py
+++ TweetData.py
@@ -13,12 +13,12 @@ import asyncio
 import aiohttp
 import pandas as pd
 
-# with open('key.json','r') as file:
-#     keys = json.load(file)
-#     bearerToken =keys['bearerToken']
+with open('key.json','r') as file:
+    keys = json.load(file)
+    bearerToken =keys['bearerToken']
 
 
-bearerToken =st.secrets['bearer_token']
+# bearerToken =st.secrets['bearer_token']
 
 class processor:
     def __init__(self) -> None: # Default 7 days TimeFrame
@@ -215,27 +215,27 @@ class contractProcessor():
         
     
     async def Priceswharehouse(self,session,poolId):
-        # headers = {
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-        #     "Accept": "application/json"
-        # }
-
         headers = {
-            "authority": "app.geckoterminal.com",
-            "accept": "application/json, text/plain, /",
-            "accept-encoding": "gzip, deflate, zstd",
-            "accept-language": "en-US,en;q=0.9",
-            "origin": "https://www.geckoterminal.com",
-            "referer": "https://www.geckoterminal.com/",
-            "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-site",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
-            "x-requested-with": "XMLHttpRequest",  # Often required
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+            "Accept": "application/json"
         }
+
+        # headers = {
+        #     "authority": "app.geckoterminal.com",
+        #     "accept": "application/json, text/plain, /",
+        #     "accept-encoding": "gzip, deflate, zstd",
+        #     "accept-language": "en-US,en;q=0.9",
+        #     "origin": "https://www.geckoterminal.com",
+        #     "referer": "https://www.geckoterminal.com/",
+        #     "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+        #     "sec-ch-ua-mobile": "?0",
+        #     "sec-ch-ua-platform": '"Windows"',
+        #     "sec-fetch-dest": "empty",
+        #     "sec-fetch-mode": "cors",
+        #     "sec-fetch-site": "same-site",
+        #     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+        #     "x-requested-with": "XMLHttpRequest",  # Often required
+        # }
         url = f'https://app.geckoterminal.com/api/p1/candlesticks/{poolId}?resolution=1&from_timestamp={self.from_timetamp}&to_timestamp={self.to_timestamp}&for_update=false&currency=usd&is_inverted=false'
         async with session.get(url=url,headers=headers) as response:
                 result = await response.json()
@@ -363,28 +363,28 @@ class contractProcessor():
         asyncio.run(self.main(timeframe))
     
     async def Fetch_PoolId_TokenId(self,session,network_id,pair):
-        # headers = {
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-        #     "Accept": "application/json"
-        # }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+            "Accept": "application/json"
+        }
 
         
-        headers = {
-                "authority": "app.geckoterminal.com",
-                "accept": "application/json, text/plain, /",
-                "accept-encoding": "gzip, deflate, zstd",
-                "accept-language": "en-US,en;q=0.9",
-                "origin": "https://www.geckoterminal.com",
-                "referer": "https://www.geckoterminal.com/",
-                "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Windows"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-site",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
-                "x-requested-with": "XMLHttpRequest",  # Often required
-            }
+        # headers = {
+        #         "authority": "app.geckoterminal.com",
+        #         "accept": "application/json, text/plain, /",
+        #         "accept-encoding": "gzip, deflate, zstd",
+        #         "accept-language": "en-US,en;q=0.9",
+        #         "origin": "https://www.geckoterminal.com",
+        #         "referer": "https://www.geckoterminal.com/",
+        #         "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+        #         "sec-ch-ua-mobile": "?0",
+        #         "sec-ch-ua-platform": '"Windows"',
+        #         "sec-fetch-dest": "empty",
+        #         "sec-fetch-mode": "cors",
+        #         "sec-fetch-site": "same-site",
+        #         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+        #         "x-requested-with": "XMLHttpRequest",  # Often required
+        #     }
         url = f"https://app.geckoterminal.com/api/p1/{network_id}/pools/{pair}?include=dex%2Cdex.network.explorers%2Cdex_link_services%2Cnetwork_link_services%2Cpairs%2Ctoken_link_services%2Ctokens.token_security_metric%2Ctokens.token_social_metric%2Ctokens.tags%2Cpool_locked_liquidities&base_token=0"
         async with session.get(url,headers=headers) as response:
            try:
@@ -398,26 +398,26 @@ class contractProcessor():
 
     async  def fetchNetworkId(self,session,address):
         # 
-        # headers = {
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-        #     "Accept": "application/json"
-        # }
         headers = {
-                "authority": "app.geckoterminal.com",
-                "accept": "application/json, text/plain, /",
-                "accept-encoding": "gzip, deflate, zstd",
-                "accept-language": "en-US,en;q=0.9",
-                "origin": "https://www.geckoterminal.com",
-                "referer": "https://www.geckoterminal.com/",
-                "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Windows"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-site",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
-                "x-requested-with": "XMLHttpRequest",  # Often required
-            }
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+            "Accept": "application/json"
+        }
+        # headers = {
+        #         "authority": "app.geckoterminal.com",
+        #         "accept": "application/json, text/plain, /",
+        #         "accept-encoding": "gzip, deflate, zstd",
+        #         "accept-language": "en-US,en;q=0.9",
+        #         "origin": "https://www.geckoterminal.com",
+        #         "referer": "https://www.geckoterminal.com/",
+        #         "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+        #         "sec-ch-ua-mobile": "?0",
+        #         "sec-ch-ua-platform": '"Windows"',
+        #         "sec-fetch-dest": "empty",
+        #         "sec-fetch-mode": "cors",
+        #         "sec-fetch-site": "same-site",
+        #         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+        #         "x-requested-with": "XMLHttpRequest",  # Often required
+        #     }
         url = f'https://app.geckoterminal.com/api/p1/search?query={address}'
         async with session.get(url,headers=headers) as response:
             try:
